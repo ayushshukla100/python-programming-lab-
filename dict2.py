79ca267288879bba9c4a504d6b6efd46bc3ead50
ayushshukla100 <106491595+ayushshukla100@users.noreply.github.com>
2022-05-30 16:14:25 +0530

    Add files via upload

---
+++ dict2.py
@@ -0,0 +1,8 @@
+# with using items
+dct={'name':'rishit','sec':'f','rolln':40}
+for key,val in dct.items():
+    print(key,val)
+print(" ")
+# without using items
+for key in dct:
+    print(key,dct[key])
